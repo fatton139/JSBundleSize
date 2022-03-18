1f271705813e74efb1f176135140e55dd988921c
fatton139 <xinyi.li13944@gmail.com>
2022-03-18 20:20:27 +1000

    Update a bunch of stuff for a simpler use case

--- README.md
+++ README.md
@@ -1,58 +1,28 @@
-# JS bundle size
-Github action for computing javascript bundle size. It calculates  & logs JavaScript code bundled size on each pull request or commit (customizable).
-With the help of this action, developers can now log & check the overall code size after compression on per commit basis & take necessary actions.
+# Comment file size on PR
 
-Upcoming features:
-Fail check if bundle size exceeds a specific value.
+Simple action to comment file sizes on a PR. Just specify the path.
 
-Stay tuned for more updates!
-
-![How to use JSBundle Size action](https://i.imgur.com/koKtvty.gif)
-
-![github actions bot comment bundle size](https://i.imgur.com/GhElRd1.png)
+You can choose to upsert the existing bot comment or create new ones after every change.
 
 ## Usage:
 
-Checkout [action.yml](./action.yml)
-
 Please check the below code for detailed usage:
+
 ```yaml
 steps:
       - uses: actions/checkout@master
-      - uses: sarthak-saxena/JSBundleSize@master
+      - run: |
+        npm install
+        npm build
+        # ... other build steps
+      - uses: fatton139/JSBundleSize@master
         with:
-          bootstrap: <Command for installing dependencies ex npm install>
-          build_command: <Command to build/bundle code>
-          dist_path: <Output path for your bundle>
+          dist_path: "./path-to-output-dir-or-file"
+          update_comment: false
           token: ${{ secrets.GITHUB_TOKEN }}
 
 ```
-
-By default github actions work on `node 12`.For a specific node version use:
-
-```yaml
-- uses: actions/setup-node@v1
-        with:
-          node-version: '10.0.0'
-```
-
-**Ex:**
-```yaml
-steps:
-      - uses: actions/checkout@master
-      - uses: actions/setup-node@v1
-        with:
-          node-version: '10.0.0'
-      - uses: sarthak-saxena/JSBundleSize@master
-        with:
-          bootstrap: npm install
-          build_command: npm run build
-          dist_path: 'dist'
-          token: ${{ secrets.GITHUB_TOKEN }}
-
-```
-
-Also check [Demo.yml](./Demo.yml) for complete configuration(on using github actions)
 
 ## License
+
 The scripts and documentation in this project are released under the [MIT License](./LICENSE)
--- index.js
+++ index.js
@@ -2,64 +2,104 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 const github = require("@actions/github");
 
-async function run() {
-  function bytesToSize(bytes) {
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-    if (bytes === 0) return "0 Byte";
-    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-    return (bytes / Math.pow(1024, i)).toFixed(2) + " " + sizes[i];
+const GITHUB_ACTIONS_USER_NAME = "github-actions[bot]";
+const GITHUB_ACTIONS_USER_TYPE = "Bot";
+const GITHUB_ACTIONS_COMMENT_START_TEXT =
+  "Bundled size for the files is listed below:";
+
+async function findComment(octokit, { owner, issue_number, repo }) {
+  for await (const { data: comments } of octokit.paginate.iterator(
+    octokit.rest.issues.listComments,
+    {
+      repo,
+      owner,
+      issue_number,
+    }
+  )) {
+    // Search each page for the comment
+    const comment = comments.find((comment) => {
+      return (
+        comment.user.login === GITHUB_ACTIONS_USER_NAME &&
+        comment.user.type === GITHUB_ACTIONS_USER_TYPE &&
+        comment.body.startsWith(GITHUB_ACTIONS_COMMENT_START_TEXT)
+      );
+    });
+
+    if (comment) {
+      return comment;
+    }
   }
+}
+
+function bytesToSize(bytes) {
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  if (bytes === 0) return "0 Byte";
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  return (bytes / Math.pow(1024, i)).toFixed(2) + " " + sizes[i];
+}
+
+async function run() {
   try {
     // --------------- octokit initialization  ---------------
     const token = core.getInput("token");
-    console.log("Initializing oktokit with token", token);
-    const octokit = new github.GitHub(token);
-    // --------------- End octokit initialization ---------------
+    const updateComment = core.getBooleanInput("update_comment");
+    const octokit = new github.getOctokit(token);
 
-    // --------------- End Build repo  ---------------
+    const dist_path = core.getInput("dist_path");
 
-    // --------------- Comment repo size  ---------------
     const outputOptions = {};
     let sizeCalOutput = "";
 
     outputOptions.listeners = {
-      stdout: data => {
+      stdout: (data) => {
         sizeCalOutput += data.toString();
       },
-      stderr: data => {
+      stderr: (data) => {
         sizeCalOutput += data.toString();
-      }
+      },
     };
     await exec.exec(`du ${dist_path}`, null, outputOptions);
     core.setOutput("size", sizeCalOutput);
-    const context = github.context,
-      pull_request = context.payload.pull_request;
+    const context = github.context;
+    const pullRequest = context.payload.pull_request;
+    const owner = context.payload.repository.owner.login;
+    const repo = context.payload.repository.name;
 
     const arrayOutput = sizeCalOutput.split("\n");
-    let result = "Bundled size for the package is listed below: \n \n";
-    arrayOutput.forEach(item => {
+    const header = "Bundled size for the files is listed below:";
+    let result = `${header} \n \n`;
+    arrayOutput.forEach((item) => {
       const i = item.split(/(\s+)/);
       if (item) {
         result += `**${i[2]}**: ${bytesToSize(parseInt(i[0]) * 1000)} \n`;
       }
     });
 
-    if (pull_request) {
-      // on pull request commit push add comment to pull request
-      octokit.issues.createComment(
-        Object.assign(Object.assign({}, context.repo), {
-          issue_number: pull_request.number,
-          body: result
-        })
-      );
+    if (pullRequest) {
+      const existingComment = await findComment(octokit, {
+        owner,
+        issue_number: pullRequest.number,
+        repo,
+      });
+      // If the comment exists then update instead of creating a new one.
+      if (existingComment && updateComment) {
+        await octokit.rest.issues.updateComment({
+          owner,
+          repo,
+          comment_id: existingComment.id,
+          body: result,
+        });
+      } else {
+        // on pull request commit push add comment to pull request
+        await octokit.rest.issues.createComment({
+          owner,
+          repo,
+          issue_number: pullRequest.number,
+          body: result,
+        });
+      }
     } else {
-      // on commit push add comment to commit
-      octokit.repos.createCommitComment(
-        Object.assign(Object.assign({}, context.repo), {
-          commit_sha: github.context.sha,
-          body: result
-        })
-      );
+      core.notice("Did not run since we're not in a PR.");
     }
 
     // --------------- End Comment repo size  ---------------
